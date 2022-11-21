36b13b5dcccca375bfbd12caf5ae30f44757855f
Perry Jorss <pjorss123@gmail.com>
2022-11-21 15:05:10 -0500

    changed text on home screen

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,9 @@ import React from "react";
 
 function Home () {
     return (
-        <div className="header-div">
-            <h1 className="seven">Find a used car with ease</h1>
-            <h2 className="second-home-header">The power of this website will be able to search through our vast databases of cars and allow users to sell or buy a new or used car!</h2>
+        <div className="seven">
+            <h1>Find a used car with ease</h1>
+            <h2 className="second-home-header">The power of this website will be able to search through our vast databases of cars and allow users to sell or buy used cars!</h2>
             <p className="home-p">We have data from multiple dealerships along partnered dealerships to give 
             everyone in the state of Maryland the chance to sell and buy cars all without having to visit 
             nultiple sites.  Here you will be able to find new and used vehicles and the names of their
