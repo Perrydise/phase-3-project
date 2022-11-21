6f0ba130cc6c32f552cd07a6a4bd47ffe5b68262
Perry Jorss <pjorss123@gmail.com>
2022-11-21 16:54:50 -0500

    added different verbage

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,8 +7,9 @@ function Home () {
         <div class="eight">
             <h1>buy or sell a used car with ease</h1>
         </div>    
-            <h2 className="second-home-header">The power of this website will be able to search through our vast databases of cars and allow users to sell or buy used cars!</h2>
-            <p className="home-p">We have data from multiple dealerships along partnered dealerships to give 
+            <h2 className="second-home-header">Welcome to the one and only used car website! Our database has tons of used cars
+            for sale and allows you to post your own with ease! Chech out our other pages for our used car form and all the cars we have for sale.</h2>
+            <p className="home-p">We have data from multiple dealerships to give 
             everyone in the state of Maryland the chance to sell and buy cars all without having to visit 
             nultiple sites.  Here you will be able to find new and used vehicles and the names of their
             owners so you can see the how many previous owners a car has had before you decide to buy!</p>
